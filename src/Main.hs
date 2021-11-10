5202ec83a068231ea95375c116132af238afd54e
chen-a <43921889+chen-a@users.noreply.github.com>
2021-11-10 03:29:45 +0000

    the beginning

--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,15 @@ module Main where
 import Control.Monad (when)
 import System.Directory (doesFileExist)
 import System.Environment (getArgs)
-import Bird.Parser
-import Bird.Printer
+-- import Bird.Parser
+-- import Bird.Printer
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+data Expr = Boolean Bool 
+    | Integer Integer 
+    | Symbols String
+    | Pairs Expr Expr
+    | Combinations [Expr]
 
 main :: IO ()
 main = do args <- getArgs
@@ -17,3 +24,16 @@ main = do args <- getArgs
 goParse, goEval :: String -> IO ()
 goParse s = putStrLn "Your implementation begins here"              
 goEval s  = putStrLn "Your implementation continues here"
+
+numbers :: GenParser Char st Int
+numbers = do
+    spaces
+    x <- many1 digit
+    return (read x :: Int)
+
+word :: GenParser Char st String
+word = do
+    spaces 
+    x <- many1 letter 
+    return x
+
